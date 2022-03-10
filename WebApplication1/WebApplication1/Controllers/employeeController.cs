6d6c67eb14d90f3a2c138c83d902de265b74b67d
KIST Student3 <student3@sddb.student>
2022-03-10 09:31:57 +0545

    fetch data from the database table

--- WebApplication1/WebApplication1/Controllers/employeeController.cs
+++ WebApplication1/WebApplication1/Controllers/employeeController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class employeeController : Controller
     {
         // GET: employee
+        EmployeeEntities db = new EmployeeEntities();
         public ActionResult Index()
         {
-            return View();
+            List<empDetail> data = db.empDetails.ToList();
+            return View(data);
         }
     }
 }
